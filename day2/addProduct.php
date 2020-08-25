e9ab0aa42ccc0ed3a2046e9b6eb8ad8ec5c2ec47
Your Name <you@example.com>
2020-08-25 23:22:45 +0700

    update

--- day2/addProduct.php
+++ day2/addProduct.php
@@ -11,7 +11,7 @@
 <body>
 <div class="container">
     <nav class="navbar navbar-expand-lg navbar-light bg-secondary">
-        <a class="navbar-brand" href="#">SMART-OSC</a>
+        <a class="navbar-brand text-white" href="#">SMART-OSC</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -19,24 +19,7 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item active">
-                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Dropdown
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="#">Action</a>
-                        <a class="dropdown-item" href="#">Another action</a>
-                        <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="#">Something else here</a>
-                    </div>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link disabled" href="#">Disabled</a>
+                    <a class="nav-link text-white" href="./index.php">Home</a>
                 </li>
             </ul>
             <form class="form-inline my-2 my-lg-0">
@@ -45,37 +28,68 @@
             </form>
         </div>
     </nav>
-    <section>
-        <table class="table">
-            <thead class="thead-light">
-                <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Sku</th>
-                    <th scope="col">Title</th>
-                    <th scope="col">Image</th>
-                    <th scope="col">Description</th>
-                    <th scope="col">Price</th>
-                    <th scope="col">Size</th>
-                    <th scope="col">Color</th>
-                    <th scope="col">Action</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>1</td>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
-                    <td>1</td>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
-                    <td>@mdo</td>
-                </tr>
-            </tbody>
-        </table>
+    <section class="mt-5">
+        <div class="card card-primary">
+            <div class="card-header">
+                <h3 class="card-title">Add Product</h3>
+            </div>
+            <form role="form">
+                <div class="card-body">
+                   <div class="row">
+                       <div class="col-sm-6 col-md-6 col-6">
+                           <div class="form-group">
+                               <label for="Sku">Sku <span class="text-danger">*</span></label>
+                               <input type="text" name="sku" class="form-control" placeholder="Enter sku ...">
+                           </div>
+                           <div class="form-group">
+                               <label for="Title">Title <span class="text-danger">*</span></label>
+                               <input type="text" name="title" class="form-control" placeholder="Enter title ...">
+                           </div>
+                           <div class="form-group">
+                               <label for="Price">Price <span class="text-danger">*</span></label>
+                               <input type="number" name="price" class="form-control" placeholder="Enter price ...">
+                           </div>
+                           <div class="form-group">
+                               <label for="Size">Size <span class="text-danger">*</span></label>
+                               <input type="text" name="size" class="form-control" placeholder="Enter size ...">
+                           </div>
+                           <div class="form-group">
+                               <label for="Color">Color <span class="text-danger">*</span></label>
+                               <input type="text" name="color" class="form-control" placeholder="Enter color ...">
+                           </div>
+                       </div>
+                       <div class="col-sm-6 col-md-6 col-6">
+                           <div class="form-group text-center">
+                               <img width="300px" id="output" src="./public/image/default-image.jpg" alt="..." class="img-thumbnail">
+                           </div>
+                           <div class="form-group">
+                               <label for="Color">Image <span class="text-danger">*</span></label>
+                               <input type="file" name="image" id="input" class="form-control" placeholder="Choose file">
+                           </div>
+                           <div class="form-group">
+                               <label>description <span class="text-danger">*</span></label>
+                               <textarea class="form-control" rows="3" placeholder="Enter description..."></textarea>
+                           </div>
+                       </div>
+                   </div>
+                </div>
+                <div class="card-footer">
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </div>
+            </form>
+        </div>
     </section>
 </div>
+
+<script>
+    document.querySelector("#input").onchange = (event) => {
+        let output = document.getElementById('output');
+        output.src = URL.createObjectURL(event.target.files[0]);
+        output.onload = function() {
+            URL.revokeObjectURL(output.src) // free memory
+        }
+    };
+</script>
 
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
--- day2/index.php
+++ day2/index.php
@@ -1,3 +1,17 @@
+<?php
+$products = [
+    [
+        "id" => 1,
+        "sku" => "MK123",
+        "title" => "san pham 1",
+        "price" => "san pham 1",
+        "description" => "abcxyz",
+        "image" => "https://images.unsplash.com/photo-1494972308805-463bc619d34e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80",
+        "size" => "M",
+        "color" => "red"
+    ]
+]
+?>
 <!doctype html>
 <html lang="en">
 <head>
@@ -7,51 +21,84 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
 </head>
 <body>
-    <div class="container">
-        <nav class="navbar navbar-expand-lg navbar-light bg-secondary">
-            <a class="navbar-brand" href="#">SMART-OSC</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
+<div class="container">
+    <nav class="navbar navbar-expand-lg navbar-light bg-secondary">
+        <a class="navbar-brand text-white" href="#">SMART-OSC</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav mr-auto">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Dropdown
-                        </a>
-                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="#">Action</a>
-                            <a class="dropdown-item" href="#">Another action</a>
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="#">Something else here</a>
-                        </div>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link disabled" href="#">Disabled</a>
-                    </li>
-                </ul>
-                <form class="form-inline my-2 my-lg-0">
-                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                </form>
-            </div>
-        </nav>
-        <section>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link text-white" href="#">Home</a>
+                </li>
+            </ul>
+            <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            </form>
+        </div>
+    </nav>
+    <section class="mt-5">
+        <table class="table">
+            <thead class="thead-light">
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Sku</th>
+                <th scope="col">Title</th>
+                <th scope="col">Image</th>
+                <th scope="col">Description</th>
+                <th scope="col">Price</th>
+                <th scope="col">Size</th>
+                <th scope="col">Color</th>
+                <th scope="col">
+                    <a class="btn btn-outline-success" href="./addProduct.php">Add</a>
+                </th>
+            </tr>
+            </thead>
+            <tbody>
+                <?
+                    if(isset($products)): foreach ($products as $key => $product):
+                ?>
+                    <tr>
+                        <td><? echo $key+1 ?></td>
+                        <td><? echo $product['sku'] ?></td>
+                        <td><? echo $product['title'] ?></td>
+                        <td>
+                            <img class="img-thumbnail" src="<? echo $product['image'] ?>" width="150px" alt="">
+                        </td>
+                        <td><? echo $product['description'] ?></td>
+                        <td><? echo $product['price'] ?></td>
+                        <td><? echo $product['size'] ?></td>
+                        <td><? echo $product['color'] ?></td>
+                        <td>
+                            <div class="btn-group">
+                                <button class="btn btn-outline-info dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    Action
+                                </button>
+                                <div class="dropdown-menu" x-placement="bottom-start" style="position: absolute; transform: translate3d(0px, 31px, 0px); top: 0px; left: 0px; will-change: transform;">
+                                    <a class="dropdown-item" href="#"><i class="fas fa-edit"></i> Edit</a>
+                                    <a class="dropdown-item" href="#"><i class="fas fa-trash-alt"></i> Delete</a>
+                                    <div class="dropdown-divider"></div>
+                                    <a class="dropdown-item" href="#"><i class="fas fa-eye"></i> Detail</a>
+                                </div>
+                            </div>
+                        </td>
+                    </tr>
+                <?
+                    endforeach; endif;
+                ?>
+            </tbody>
+        </table>
+    </section>
+</div>
 
-        </section>
-    </div>
-
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </body>
 </html>
